0ae12945b8f75eb441b3c7acbb4bed2ea4cea723
Freyanord <annabashina135@gmail.com>
2023-04-02 14:06:55 +0300

    Correct

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const form = document.querySelector('#task_form');
 const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksGroup');
+const emptyList = document.querySelector('#emptyList');
 
 form.addEventListener('submit', function (event) {
     //отменяем отправку формы
@@ -7,7 +9,18 @@ form.addEventListener('submit', function (event) {
     //достаем текст задачи из поля ввода
     const taskText = taskInput.value;
     //формируем разметку для новой задачи
-    
+    const taskHTML = `<li class="t-1">
+                        <span class="task_title">${taskText}</span>
+                        <input type="checkbox"/>  
+                    </li>`;
+//добавляем задачу на страницу
+tasksGroup.insertAdjacentHTML('beforeend', taskHTML); 
+//очищаем поле ввода 
+taskInput.value = "";
+taskInput.focus();
 
+if(tasksGroup.children.length > 1) {
+    emptyList.classList.add('none')
+};
 
-})
+});
